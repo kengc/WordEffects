be13fc2ed86349df62b9827ff409122fb067663c
kevin Cleathero <kengc@hotmail.com>
2017-05-29 15:04:32 -0700

    Base line checking Added example code and is working

--- Word Effects/main.m
+++ Word Effects/main.m
@@ -10,8 +10,27 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+      
+//        The app will take in two pieces of data from the user:
+//        
+//        a number, to indicate which operation to do
+//            a string, to operate on
+//            Wrap the whole app in an infinite while loop, so users can do multiple operations.
+//                
+//                Be sure to print out the menu of options at the start of each loop of the app.
+//                
+//                As you go through the steps of this assignment, be sure to commit regularly, and push your code to GitHub.
+    
+        char inputChars[255];
+        
+        printf("Input a string: ");
+        fgets(inputChars, 255, stdin);
+        printf("Your string is %s\n", inputChars);
+        
+        NSString *inputString = [NSString stringWithUTF8String:inputChars];
+        
+        NSLog(@"Input was: %@", inputString);
+        
     }
     return 0;
 }
