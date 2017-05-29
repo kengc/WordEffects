8eaa06f9a6da339b9627ae59c915091099fb5429
kevin Cleathero <kengc@hotmail.com>
2017-05-29 14:56:07 -0700

    set up word effects

---
+++ Word Effects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Word Effects
+//
+//  Created by Kevin Cleathero on 2017-05-29.
+//  Copyright © 2017 Kevin Cleathero. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
