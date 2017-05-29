afff9d5e3f20d23c368fff159c877b55cd858194
kevin Cleathero <kengc@hotmail.com>
2017-05-29 16:57:15 -0700

    implimented first three switch cases

--- Word Effects/main.m
+++ Word Effects/main.m
@@ -29,13 +29,60 @@ int main(int argc, const char * argv[]) {
         fgets(&inputNum, 255, stdin);
         printf("Your number is %c\n", inputNum);
         
-        printf("Input a string: ");
-        fgets(inputChars, 255, stdin);
-        printf("Your string is %s\n", inputChars);
+//        printf("Input a string: ");
+//        fgets(inputChars, 255, stdin);
+//        printf("Your string is %s\n", inputChars);
         
-        NSString *inputString = [NSString stringWithUTF8String:inputChars];
+       
         
-        NSLog(@"Input was: %@", inputString);
+        //NSLog(@"Input was: %@", inputString);
+        
+        switch (inputNum) {
+            case '1':{
+                //1. Uppercase
+                printf("Input a string in all lower case: ");
+                fgets(inputChars, 255, stdin);
+                NSString *inputString = [NSString stringWithUTF8String:inputChars];
+                NSString *upperCase = [inputString uppercaseString];
+                NSLog(@"Input as upper case: %@", upperCase);
+            }
+                break;
+            case '2':{
+                //2. Lowercase
+                printf("Input a string in all UPPER case: ");
+                fgets(inputChars, 255, stdin);
+                NSString *inputString = [NSString stringWithUTF8String:inputChars];
+                NSString *lowerCase = [inputString lowercaseString];
+                NSLog(@"Input as lower case:  %@", lowerCase);
+            }
+                break;
+            case '3':{
+                //3. Numberize
+                printf("Input another number: ");
+                fgets(inputChars, 255, stdin);
+                NSString *chars = [NSString stringWithUTF8String:inputChars];
+                NSString *trimmed = [[chars componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]] componentsJoinedByString:@""];
+                NSNumberFormatter *f = [[NSNumberFormatter alloc]init];
+                f.numberStyle = NSNumberFormatterDecimalStyle;
+                NSNumber *myNumber = [f numberFromString:trimmed];
+                
+                NSLog(@"string as a number: %@",myNumber);//NSString to NSNumber
+            }
+                break;
+            case 4:
+                //4. Canadianize
+                break;
+            case 5:
+                //5. Respond
+                break;
+            case 6:
+                //6. De-Space-It
+                break;
+                
+            default:
+                break;
+        }
+        
         
     }
     return 0;
